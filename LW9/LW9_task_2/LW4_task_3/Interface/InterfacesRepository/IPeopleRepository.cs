198582597203f7f4db55101bbdd107ea60d41787
BashukOleksii <oleksiichort@gmail.com>
2025-11-17 19:23:41 +0200

    Add task2 LW9 + part of program to deploy

---
+++ LW9/LW9_task_2/LW4_task_3/Interface/InterfacesRepository/IPeopleRepository.cs
@@ -0,0 +1,9 @@
+﻿using LW4_task_3.Models.Entities;
+
+namespace LW4_task_3.Interface.InterfacesRepository
+{
+    public interface IPeopleRepository : IRepository<PeopleItem>
+    {
+        Task<IEnumerable<PeopleItem>?> GetPeoplesItemsAsync(string? name, string? email);
+    }
+}
